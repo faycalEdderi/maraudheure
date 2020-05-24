3c76b18f8bcab403c443da68e0675d84ef3659db
faycalEdderi <faycmx@live.fr>
2020-05-24 23:53:33 +0200

    first commit

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class MyUsers(User):
+
+    phone_number =  models.CharField(
+        max_length=100)
+
+
+    def __str__(self):
+        return self.last_name  + " " +  self.first_name
+
+    class Meta:
+        verbose_name = 'MyUser'
+
