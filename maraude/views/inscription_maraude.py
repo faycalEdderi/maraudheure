ebc85a4ab69e1df4e7ce52f14433fd365d193240
faycalEdderi <faycmx@live.fr>
2020-06-26 22:09:27 +0200

    description de maraude

--- maraude/views/inscription_maraude.py
+++ maraude/views/inscription_maraude.py
@@ -18,5 +18,30 @@ def inscription_maraude(request, pk=None):
   
     return redirect('home')
     
+# fonction d'inscription a une maraude pour un bénévole
+def desinscription_maraude(request, pk=None):
+    try:
+
+        user = request.user
+        benevole = Benevole.objects.get(id=user.id)
+        maraude = Maraude.objects.get(id=pk)
+        if user:
+
+            maraude.benevole.remove(benevole)
+        
+            maraude.save()
+            messages.add_message(request, messages.INFO, 'Vous etes désinscrit de la maraude')
+            return redirect('home')
+        else:
+            messages.error(request, "Une erreur s'est produite : votre compte n'a pas été supprimé")
+    
+            return redirect('desinscription_maraude')
+    except:
+        messages.error(request, "Une erreur s'est produite : Vous n'etes pas autorisé à acceder à cette page")
+        return redirect('home')
+    
+
+
+
 
 
